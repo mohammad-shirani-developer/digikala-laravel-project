d9247c69688feed57b67331315eec1cabae61540
Mohammad-Shrani <mohammad.shirani.developer@gmail.com>
2025-01-05 16:03:41 +0330

    submit product and validation

--- app/Livewire/Admin/Product/Create.php
+++ app/Livewire/Admin/Product/Create.php
@@ -3,7 +3,9 @@
 namespace App\Livewire\Admin\Product;
 
 use App\Models\Category;
+use App\Models\Product;
 use App\Models\Seller;
+use Illuminate\Support\Facades\Validator;
 use Livewire\Component;
 use Illuminate\Support\Str;
 
@@ -15,6 +17,8 @@ class Create extends Component
     public $name;
     public $slug;
 
+    public $productId;
+
     public function mount()
     {
         $this->categories = Category::all();
@@ -24,6 +28,44 @@ class Create extends Component
     public function updatedName()
     {
         $this->slug = Str::slug($this->name, '-', null);
+    }
+
+    public function submit($FormData, Product $product)
+    {
+
+        if (isset($FormData['featured'])) {
+            $FormData['featured'] = true;
+        } else {
+            $FormData['featured'] = false;
+        }
+
+        // dd($FormData);
+
+        $validator = Validator::make($FormData, [
+            'name' => 'required|string',
+            'slug' => 'required|string',
+            'meta_title' => 'nullable|string',
+            'meta_description' => 'nullable|string',
+            'price' => 'required|integer',
+            'discount' => 'nullable|integer',
+            'stock' => 'required|integer',
+            'featured' => 'nullable|boolean',
+            'discount_duration' => 'nullable|string',
+            'sellerId' => 'nullable|exists:sellers,id',
+            'categoryId' => 'required|exists:categories,id',
+        ], [
+            '*.required' => 'فیلد ضروری است.',
+            '*.string' => 'فرمت اشتباه است ',
+            '*.integer' => 'این فیلد باید فرمت عددی باشد ',
+            '*.min' => 'حداکثر تعداد کاراکترها : 50',
+            'categoryId.exists' => 'دسته بندی نامعتبر است.',
+            'sellerId.exists' => 'فروشنده نامعتبر است.'
+        ]);
+        $validator->validate();
+        $this->resetValidation();
+        $product->submit($FormData, $this->productId);
+        $this->reset();
+        $this->dispatch('success', 'عملیات با موفقیت انجام شد');
     }
 
     public function render()
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,9 +2,47 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function submit($FormData, $productId)
+    {
+        $product = $this->submitToProduct($FormData, $productId);
+        $this->submitToSeoItem($FormData, $product->id);
+    }
+
+    public function submitToProduct($FormData, $productId)
+    {
+        return Product::query()->updateOrCreate([
+            'id' => $productId,
+        ], [
+            'name' => $FormData['name'],
+            'price' => $FormData['price'],
+            'discount' => $FormData['discount'],
+            'stock' => $FormData['stock'],
+            'featured' => $FormData['featured'],
+            'discount_duration' => $FormData['discount_duration'],
+            'category_id' => $FormData['categoryId'],
+            'seller_id' => $FormData['sellerId']
+
+        ]);
+    }
+
+    public function submitToSeoItem($FormData, $productId)
+    {
+        SeoItem::query()->updateOrCreate([
+            'type' => 'product',
+            'ref_id' => $productId
+        ], [
+            'slug' => $FormData['slug'],
+            'meta_title' => $FormData['meta_title'],
+            'meta_description' => $FormData['meta_description'],
+        ]);
+    }
 }
