cfcd2b4f2908f59ed3c70e5fc5e066e411cac5f6
Mohammad-Shrani <mohammad.shirani.developer@gmail.com>
2025-01-07 17:02:44 +0330

    refactor product list component

--- resources/views/livewire/admin/product/index.blade.php
+++ resources/views/livewire/admin/product/index.blade.php
@@ -16,10 +16,12 @@
     @endif
     <div class="statbox widget box box-shadow">
         <div class="widget-header">
-            <div class="row">
-                <div class="col-xl-12 col-md-12 col-sm-12 col-12">
-                    <h4>لیست محصولات </h4>
-                </div>
+            <div class="d-flex justify-content-between align-items-center">
+                <h4>لیست محصولات </h4>
+                <a href="{{ route('admin.product.create') }}" class="btn btn-outline-success mb-2 me-4"> افزودن محصول
+                    جدید</a>
+
+
             </div>
         </div>
         <div class="widget-content widget-content-area">
@@ -29,6 +31,7 @@
                     <thead>
                         <tr>
                             <th scope="col">#</th>
+                            <th scope="col">کد محصول</th>
                             <th scope="col">تصویر محصول</th>
                             <th scope="col">نام محصول</th>
                             <th scope="col">نام دسته بندی</th>
@@ -41,6 +44,9 @@
                             <tr>
                                 <td>
                                     {{ $loop->iteration + $products->firstItem() - 1 }}
+                                </td>
+                                <td>
+                                    {{ $product->p_code }}
                                 </td>
                                 <td>
                                     <img src="/products/{{ $product->id }}/small/{{ @$product->coverImage->path }}"
