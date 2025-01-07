cb000c03def709678dc14fb49dc67308bb0da464
Mohammad-Shrani <mohammad.shirani.developer@gmail.com>
2025-01-07 17:34:51 +0330

    delete product is done

--- app/Livewire/Admin/Product/Index.php
+++ app/Livewire/Admin/Product/Index.php
@@ -3,10 +3,22 @@
 namespace App\Livewire\Admin\Product;
 
 use App\Models\Product;
+use App\Models\ProductImage;
+use App\Models\SeoItem;
 use Livewire\Component;
 
 class Index extends Component
 {
+    public function delete($productId)
+    {
+        $product = Product::query()->where('id', $productId)->first();
+        if ($product) {
+            $product->delete();
+        }
+        ProductImage::query()->where('product_id', $productId)->delete();
+        SeoItem::query()->where('ref_id', $productId)->delete();
+    }
+
     public function render()
     {
         $products = Product::query()->with('category', 'coverImage')->latest()->paginate(10);
