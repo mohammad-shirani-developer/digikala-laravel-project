a41dba6d326d440f1ff5a6eefb93ce8bc5066888
Mohammad-Shrani <mohammad.shirani.developer@gmail.com>
2025-01-07 17:40:12 +0330

    insert Softdelete in model

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class OrderItem extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Facades\DB;
 use Intervention\Image\Drivers\Gd\Driver;
 use Intervention\Image\ImageManager;
@@ -12,7 +13,7 @@ use function PHPUnit\Framework\fileExists;
 
 class Product extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $guarded = [];
 
