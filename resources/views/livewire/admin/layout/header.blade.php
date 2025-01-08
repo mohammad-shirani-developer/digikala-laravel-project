f72d79f365bc4869638832528c44c55b45dd8a07
Mohammad-Shrani <mohammad.shirani.developer@gmail.com>
2025-01-08 16:23:10 +0330

    add header image and route sider bar product in admin panel

--- resources/views/livewire/admin/layout/header.blade.php
+++ resources/views/livewire/admin/layout/header.blade.php
@@ -4,15 +4,12 @@
         <ul class="navbar-item theme-brand flex-row  text-center">
             <li class="nav-item theme-logo">
                 <a href="index.html">
-                    <img src="/admin-asset/src/assets/img/logo2.svg" class="navbar-logo" alt="logo">
+                    <img src="/admin-asset/digikala-logo-admin.png" alt="logo">
                 </a>
-            </li>
-            <li class="nav-item theme-text">
-                <a href="index.html" class="nav-link"> CORK </a>
             </li>
         </ul>
 
-        <div class="search-animated toggle-search">
+        {{-- <div class="search-animated toggle-search">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
                 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
                 class="feather feather-search">
@@ -31,7 +28,7 @@
                 </div>
             </form>
             <span class="badge badge-secondary">Ctrl + /</span>
-        </div>
+        </div> --}}
 
         <ul class="navbar-item flex-row ms-lg-auto ms-0 action-area">
 
