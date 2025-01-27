403769b5b38b4676b8eacc160cd46350904c58d8
Mohammad-Shrani <mohammad.shirani.developer@gmail.com>
2025-01-27 14:33:54 +0330

    create zebalVerify in trait paymentGetWay

--- app/Livewire/Client/Payment/Callback.php
+++ app/Livewire/Client/Payment/Callback.php
@@ -2,10 +2,17 @@
 
 namespace App\Livewire\Client\Payment;
 
+use App\Traits\PaymentGetWay;
 use Livewire\Component;
 
 class Callback extends Component
 {
+    use PaymentGetWay;
+
+    public function mount()
+    {
+        $this->zibalVerify();
+    }
     public function render()
     {
         return view('livewire.client.payment.callback');
--- app/Traits/PaymentGetWay.php
+++ app/Traits/PaymentGetWay.php
@@ -8,28 +8,54 @@ use App\Classes\Zibal;
 
 trait PaymentGetWay
 {
-   protected function zibalRequest($amount)
-   {
-    $zibal=new Zibal();
-
-    $parameters = array(
-        "merchant"=> 'zibal',//required
-        "callbackUrl"=> route('client.payment.callback'),//required
-        "amount"=> $amount,//required
-    
-        "orderId"=> time(),//optional
-        "mobile"=> "09120000000",//optional for mpg
-    );
-    
-    $response =$zibal->postToZibal('request', $parameters);
-  
-    if ($response->result == 100)
+    protected function zibalRequest($amount)
     {
-        $startGateWayUrl = "https://gateway.zibal.ir/start/".$response->trackId;
-        return redirect($startGateWayUrl);
+        $zibal = new Zibal();
+
+        $parameters = array(
+            "merchant" => 'zibal', //required
+            "callbackUrl" => route('client.payment.callback'), //required
+            "amount" => $amount * 10, //required
+
+            "orderId" => time(), //optional
+            "mobile" => "09120000000", //optional for mpg
+        );
+
+        $response = $zibal->postToZibal('request', $parameters);
+
+        if ($response->result == 100) {
+            $startGateWayUrl = "https://gateway.zibal.ir/start/" . $response->trackId;
+            return redirect($startGateWayUrl);
+        } else {
+            throw new \Exception('Error Code :' . $response->result . '--Message :' . $response->message);
+        }
     }
-    else{
-        Throw new \Exception('Error Code :'.$response->result.'--Message :'.$response->message);
+
+    protected function zibalVerify()
+    {
+        $zibal = new Zibal();
+
+        if ($_GET['success'] == 1) {
+            echo "شناسه سفارش: " . $_GET['orderId'] . "<br>";
+
+            //start verfication
+            $parameters = array(
+                "merchant" => 'zibal', //required
+                "trackId" => $_GET['trackId'], //required
+
+            );
+
+            $response = $zibal->postToZibal('verify', $parameters);
+
+            if ($response->result == 100) {
+                echo "<pre>"; //for pretty view :)
+                var_dump($response);
+                //update database or something else
+            } else {
+                throw new \Exception('result :' . $response->result . '--Message :' . $response->message);
+            }
+        } else {
+            echo "پرداخت با شکست مواجه شد.";
+        }
     }
-   }
 }
